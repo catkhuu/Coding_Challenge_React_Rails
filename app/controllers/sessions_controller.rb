a96be8a87bc98bf02931519b0bba5c8b47abf8b5
catkhuu <sunshine.dumbo@gmail.com>
2017-03-27 22:25:30 -0400

    Create routes and methods for sessions_controller.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by(username: params[:sessions][:username])
+    if @user && @user.authenticate(params[:sessions][:password])
+      session[:user_id] = @user.id
+      redirect_to user_profile_path(@user, @user.profile)
+    else
+      @errors = ['Invalid email or password']
+      render 'new'
+    end
+  end
+
+  def destroy
+    session.clear
+    redirect_to root_path
+  end
+end
