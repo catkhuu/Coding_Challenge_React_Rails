e7acab6f6e9e904676c3aa34f36484381f372741
catkhuu <sunshine.dumbo@gmail.com>
2017-03-29 13:02:17 -0400

    Create before_action for generate_proxy_request.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :generate_proxy_request
+  include SessionsHelper
+
+  protected
+
+  def generate_proxy_request
+    @proxy_request = Request.new
+  end
 end
