502918b99207cfc7a1221008abb68ce8089d3258
catkhuu <sunshine.dumbo@gmail.com>
2017-03-29 13:05:16 -0400

    Remove UsersController and remove users routes [:show, :update]. Fix routing for adding/removing recipe from menu. Add index for recipe.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :menus do
+    resources :recipes,
+    controller: 'menu_recipes',
+    only: [:new, :create, :destroy],
+    param: :recipe_id
+  end
+  resources :recipes
+  resources :sessions, only: [:new, :create, :destroy]
+
+  root 'pages#index'
 end
